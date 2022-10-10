942bd3a3f11896a6bace4fb9136bf2dfe0cfbec3
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2022-10-10 20:49:43 -0300

    Feriado v.4

--- feriados/core/tests.py
+++ feriados/core/tests.py
@@ -22,3 +22,32 @@ class TirantesTest(TestCase):
 
     def test_texto(self):
         self.assertContains(self.resp, 'Tiradentes')
+
+
+from core.models import FeriadoModel
+from datetime import datetime
+
+class FeriadoModelTest(TestCase):
+    def setUp(self):
+        self.feriado = 'Natal'
+        self.mes = 12
+        self.dia = 25
+        self.cadastro = FeriadoModel(
+            nome=self.feriado,
+            dia=self.dia,
+            mes=self.mes,
+        )
+        self.cadastro.save()
+    def test_created(self):
+        self.assertTrue(FeriadoModel.objects.exists())
+
+    def test_modificado_em(self):
+        self.assertIsInstance(self.cadastro.modificado_em, datetime)
+
+    def test_nome_feriado(self):
+        nome = self.cadastro.__dict__.get('nome', '')
+        self.assertEqual(nome, self.feriado)
+
+    def test_dia_feriado(self):
+        dia = self.cadastro.__dict__.get('dia', '')
+        self.assertEqual(dia, self.dia)
