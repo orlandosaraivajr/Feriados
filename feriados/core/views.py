cd1a41fc12d969729c6b3258e3a1206b1dcdaf9e
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2022-10-10 19:42:59 -0300

    Feriados v.1

--- feriados/core/views.py
+++ feriados/core/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def natal(request):
+    return HttpResponse("<h1><center>Não é Natal</center></h1>")
