a6c375f70317b1752d590c52b7b91159e9543226
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2022-10-10 20:03:37 -0300

    Feriado v.2

--- feriados/core/tests.py
+++ feriados/core/tests.py
@@ -9,3 +9,13 @@ class NatalTest(TestCase):
 
     def test_texto(self):
         self.assertContains(self.resp, 'Natal')
+
+class TirantesTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/tiradentes')
+
+    def test_200_response(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_texto(self):
+        self.assertContains(self.resp, 'Tiradentes')
--- feriados/core/views.py
+++ feriados/core/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def natal(request):
-    return HttpResponse("<h1><center>Não é Natal</center></h1>")
+    return render(request, 'natal.html')
+
+def tiradentes(request):
+    return HttpResponse('<h1>Tiradentes</h1>')
