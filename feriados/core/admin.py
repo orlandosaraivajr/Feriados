78b5d9caeff5d7b86ea6a5e9ace1378f24159812
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2022-10-17 12:03:38 -0300

    Refatorando testes e view. Modelo feriado add no admin

--- feriados/core/admin.py
+++ feriados/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from core.models import FeriadoModel
 
 # Register your models here.
+
+admin.site.register(FeriadoModel)
--- feriados/core/urls.py
+++ feriados/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from . import views
 
+
 urlpatterns = [
-    path('',views.feriado),
-    path('tiradentes',views.tiradentes),
+    path('',views.feriado, name='feriado'),
+    path('tiradentes',views.tiradentes, name='tiradentes'),
 ]
